1dfbd6ec8f5292fa683b98a0eaa0ba9e7256bf1d
metastable-void <git@yuka.h09.eu>
2025-11-26 15:48:42 +0900

    create database if not exists

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,12 +2,15 @@ pub mod user_repo;
 
 // src/db/mod.rs (add this)
 use sqlx::SqlitePool;
+use sqlx::sqlite::SqliteConnectOptions;
 
 pub type Db = SqlitePool;
 
 pub async fn init_db(path: &std::path::Path) -> anyhow::Result<Db> {
-    let url = format!("sqlite:{}", path.display());
-    let pool = SqlitePool::connect(&url).await?;
+    let options = SqliteConnectOptions::new()
+        .filename(path)
+        .create_if_missing(true);
+    let pool = SqlitePool::connect_with(options).await?;
     sqlx::migrate!().run(&pool).await?;
     Ok(pool)
 }
