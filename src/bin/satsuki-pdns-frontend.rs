c5f2f8dbb9dd1bb0e9204f853d5cb88f5fe3b660
metastable-void <git@yuka.h09.eu>
2025-11-26 14:05:33 +0900

    embed files

--- src/bin/satsuki-pdns-frontend.rs
+++ src/bin/satsuki-pdns-frontend.rs
@@ -1,3 +1,10 @@
+
+use mirams_proc_macros::generate_recursive_dir_content_list;
+
+// list of pairs (path, content)
+// example entry: ("./index.html", "<p>Hello!</p>")
+const FILES: &'static [(&'static str, &'static [u8])] = &generate_recursive_dir_content_list!("./dist");
+
 fn main() {
     todo!("unimplemented");
 }
