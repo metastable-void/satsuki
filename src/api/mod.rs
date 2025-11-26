f34325c3bac50a3432c81ab5eea002eb256e755b
metastable-void <git@yuka.h09.eu>
2025-11-26 13:46:56 +0900

    cors

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -7,9 +7,15 @@ use axum::{
     Extension, Router,
     routing::{get, post},
 };
+use tower_http::cors::{Any, CorsLayer};
 
 pub fn create_router(state: SharedState) -> Router {
     use crate::api::{dns, profile, public};
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
 
     Router::new()
         // public
@@ -21,5 +27,6 @@ pub fn create_router(state: SharedState) -> Router {
         .route("/api/ns-mode/internal", post(profile::set_ns_internal))
         .route("/api/ns-mode/external", post(profile::set_ns_external))
         .route("/api/profile", get(profile::get_profile))
+        .layer(cors)
         .layer(Extension(state))
 }
